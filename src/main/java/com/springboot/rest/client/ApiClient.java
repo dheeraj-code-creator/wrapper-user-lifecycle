5aaa25b23fad97c2dbd2157a8ed4c4051d7a444a
dheeraj-code-creator <54579746+dheeraj-code-creator@users.noreply.github.com>
2020-06-10 21:36:30 +0530

    Update ApiClient.java
    updated

--- src/main/java/com/springboot/rest/client/ApiClient.java
+++ src/main/java/com/springboot/rest/client/ApiClient.java
@@ -1,13 +1,16 @@
-  package com.springboot.rest.client;
+package com.springboot.rest.client;
   import java.net.URI;
   
   public interface ApiClient {
   
-  <R> R postOperation(URI uri, Object requestObject, Class<R> rClasss);
+  <R> R postOperation(URI postUri, Object requestObject, Class<R> rClasss);
   
-  <R> R getOperation(URI uri, Class<R> rClasss);
+  <R> R getOperation(URI getUri, Class<R> rClasss);
   
-  void putOperation(URI uri, Object requestObject);
+  void putOperation(URI putUri, Object requestObject, String userId, Class<Object> class1);
+
+  <R> R getOperationByUserId(URI getUriById, String userId, Class<R> rClass);
+
+  void deleteOperation(URI deleteUri, String userId);
   
   }
- 
