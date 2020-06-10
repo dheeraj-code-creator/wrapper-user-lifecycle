aa13a8493dbebad8339b85d6f829bc096832b8f2
dheeraj-code-creator <54579746+dheeraj-code-creator@users.noreply.github.com>
2020-06-10 21:33:33 +0530

    Update UserDetailsController.java
    updated

--- src/main/java/com/springboot/rest/controller/UserDetailsController.java
+++ src/main/java/com/springboot/rest/controller/UserDetailsController.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,5 +32,21 @@ public class UserDetailsController {
 	@GetMapping(value = "/allUser", produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<UserDto> getUserDetails() {
 		return userDetailsService.getAllUserDetails();
+	}
+	
+	@PutMapping(value = "/updateUser/{userId}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<HttpStatus> updateExistingUser(@RequestBody UserDto userdto, @PathVariable("userId")String userId) {
+		userDetailsService.updateUser(userdto, userId);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+	
+	@GetMapping(value = "/{userId}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public UserDto getUserById(@PathVariable("userId") String userId) {
+		return userDetailsService.getUserByUserId(userId);
+	}
+	
+	@DeleteMapping(value = "/delete/{userId}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public void deleteUser(@PathVariable(value = "userId") String userId) {
+		userDetailsService.deleteById(userId);
 	}
 }
